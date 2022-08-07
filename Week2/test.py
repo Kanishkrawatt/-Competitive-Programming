4afe07eb02dc04ee25fa2b7f2d2bf1a25ae7d6a6
Kanishk <rawat27kanishk@gmail.com>
2022-08-07 14:07:32 +0530

    Weeks2:Assignment 1

---
+++ Week2/test.py
@@ -0,0 +1,10 @@
+x = [[3,5],"mimsy",2,"borogove",1]  # Statement 1
+y = x[0:50]                          # Statement 2
+z = y                                # Statement 3
+w = x                                # Statement 4
+x[1] = x[1][:5] + 'ery'              # Statement 5
+y[1] = 4                             # Statement 6
+w[1][:3] = 'fea'                     # Statement 7
+z[4] = 42                            # Statement 8
+x[0][0] = 5555                       # Statement 9
+a = (x[3][1] == 1)                   # Statement 10
